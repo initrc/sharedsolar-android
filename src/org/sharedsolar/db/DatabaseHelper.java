d8e949bd7bb5d5a327743ac91e28c51f3d39d71a
David <DavidSnooker@gmail.com>
2011-06-22 16:28:38 -0400

    tech get token, vendor sell token, drop credit table

--- src/org/sharedsolar/db/DatabaseHelper.java
+++ src/org/sharedsolar/db/DatabaseHelper.java
@@ -10,20 +10,16 @@ public class DatabaseHelper extends SQLiteOpenHelper {
 	private static final String DATABASE_NAME = "sharedsolar";
 	private static final int DATABASE_VERSION = 1;
 	public static final String USER_TABLE = "user";
-	public static final String CREDIT_TABLE = "credit";
 	public static final String TOKEN_TABLE = "token";
 	
 	private static final String CREATE_USER = "create table " 
 		+ USER_TABLE + " (_id integer primary key autoincrement, "
 		+ "username text not null, password text not null);";
-	private static final String CREATE_CREDIT = "create table " 
-		+ CREDIT_TABLE + " (_id integer primary key autoincrement, "
-		+ "denomination integer not null, count integer not null);";
 	private static final String CREATE_TOKEN = "create table " 
 		+ TOKEN_TABLE + " (_id integer primary key autoincrement, "
 		+ "token_id integer not null, denomination integer not null, "
-		+ "state integer not null, meter_id text, "
-		+ "circuit_id text, timestamp timestamp default CURRENT_TIMESTAMP);";
+		+ "state integer not null, account_id text, "
+		+ "timestamp timestamp default CURRENT_TIMESTAMP);";
 	
 	public DatabaseHelper(Context context) {
 		super(context, DATABASE_NAME, null, DATABASE_VERSION);
@@ -31,7 +27,6 @@ public class DatabaseHelper extends SQLiteOpenHelper {
 
 	public void onCreate(SQLiteDatabase db) {
 		db.execSQL(CREATE_USER);
-		db.execSQL(CREATE_CREDIT);
 		db.execSQL(CREATE_TOKEN);
 	}
 
@@ -39,7 +34,7 @@ public class DatabaseHelper extends SQLiteOpenHelper {
 		Log.w(DatabaseHelper.class.getName(), 
 				"Upgrading database from version " + oldVersion + " to " + newVersion);
 		db.execSQL("DROP TABLE IF EXISTS " + USER_TABLE);
-		db.execSQL("DROP TABLE IF EXISTS " + CREDIT_TABLE);
+		db.execSQL("DROP TABLE IF EXISTS " + TOKEN_TABLE);
 		onCreate(db);
 	}
 }
