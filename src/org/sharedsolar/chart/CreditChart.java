6d1d6727164bcd8f063134a530bf38e5de6e98ca
David <DavidSnooker@gmail.com>
2011-07-05 18:44:55 -0400

    fixed more strings and styles of chart

--- src/org/sharedsolar/chart/CreditChart.java
+++ src/org/sharedsolar/chart/CreditChart.java
@@ -42,7 +42,7 @@ public class CreditChart extends AbstractDemoChart {
 	public Intent execute(Context context) {
 		int num = 20;
 		// title
-		String[] titles = new String[] {context.getString(R.string.today)};
+		String[] titles = new String[] {context.getString(R.string.current)};
 		
 		// value
 		List<double[]> values = new ArrayList<double[]>();
@@ -63,11 +63,13 @@ public class CreditChart extends AbstractDemoChart {
 		renderer.setDisplayChartValues(true);
 		renderer.setTextTypeface("sans-serif", Typeface.NORMAL);
 		for (int i = 0; i < num; i++)
-			renderer.addTextLabel(i+1, "account" + (i+1));
-		
+			renderer.addTextLabel(i+1, "acnt" + (i+1));
+		String chartTitleLabel = context.getString(R.string.availableCredit);
+		String chartYLabel = context.getString(R.string.credit);
+				
 		// settings
-		setChartSettings(renderer, "Energy - Today", "", "kWh",
-				0.5, 21, 0, 105, Color.GRAY, Color.LTGRAY);
+		setChartSettings(renderer, chartTitleLabel, "", chartYLabel,
+				0, 21, 0, 105, Color.GRAY, Color.LTGRAY);
 		return ChartFactory.getBarChartIntent(context,
 				buildBarDataset(titles, values), renderer, Type.DEFAULT);
 	}
